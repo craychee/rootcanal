6793d3589c19f048b236d4d036c9bbcdf13f74d1
Michelle Krejci <krejci@palantir.net>
2015-09-04 13:28:22 -0400

    Major refactor to allow for the loading of yml.

--- src/Rootcanal/Config/Config.php
+++ src/Rootcanal/Config/Config.php
@@ -1,84 +1,117 @@
 <?php
 
 namespace Rootcanal\Config;
+use Rootcanal\Config\ConfigLoader;
+use Rootcanal\Config\ConfigTree;
+use Symfony\Component\Config\Definition\Processor;
 
 class Config
 {
-    /*
-     * @var string|bool
-     */
-    private $productionMode;
+    protected $configuration = null;
+    protected $productionMode;
 
-    /*
-     * @var string
-     */
-    private $destination;
-
-    /*
-     * @var string
-     */
-    private $modulePath;
-
-    /*
-     * @var string
-     */
-    private $themePath;
-
-    /*
-     * @var string
-     */
-    private $drushPath;
-
-    /*
-     * @var string
-     */
-    private $profilePath;
-
-    /*
-     * @var string
-     */
-    private $filesPublicPath;
-
-    /*
-     * @var string
-     */
-    private $filesPrivatePath;
-
-    /*
-     * @var string
-     */
-    private $settingsPath;
-
-    /*
-     * @var string
-     */
-    private $vendorPath;
-
-    public function __construct(
-        $productionMode,
-        $destination,
-        $modulePath,
-        $themePath,
-        $drushPath,
-        $profilePath,
-        $filesPublicPath,
-        $filesPrivatePath,
-        $settingsPath
-    )
+    public function __construct($productionMode)
     {
         $this->productionMode   = $productionMode;
-        $this->destination      = $this->getDestination($destination);
-        $this->modulePath       = $modulePath;
-        $this->themePath        = $themePath;
-        $this->drushPath        = $drushPath;
-        $this->profilePath      = $profilePath;
-        $this->filesPublicPath  = $filesPublicPath;
-        $this->filesPrivatePath = $filesPrivatePath;
-        $this->settingsPath     = $settingsPath;
-        $this->vendorPath       =
-            DIRECTORY_SEPARATOR . 'sites' .
-            DIRECTORY_SEPARATOR . 'default' .
-            DIRECTORY_SEPARATOR . 'vendor';
+    }
+
+    public function getConfig()
+    {
+        return $this->configuration ?: $this->setConfig();
+    }
+
+    public function getDestinationConfig()
+    {
+        $config = $this->getConfig();
+        $destination = [];
+
+        foreach ($config['destination_paths'] as $key => $config) {
+            $destination[$key] = $this->getFullDestination() . DIRECTORY_SEPARATOR . $config;
+        }
+
+        // Set the custom destination off of the module destination.
+        $destination['custom'] = $destination['module'] . DIRECTORY_SEPARATOR . '%s';
+
+        return $destination;
+    }
+
+    public function getSourceConfig()
+    {
+        return $this->getConfig()['source_paths'];
+    }
+
+    public function getFinderConfig()
+    {
+        return $this->getConfig()['finder_settings'];
+    }
+
+    public function setConfig()
+    {
+        $config = $this->createConfigLoader();
+        $processor = $this->createProcessor();
+        $tree = $this->createConfigTree();
+
+        $this->configuration = $processor->processConfiguration(
+            $tree,
+            $config->loadConfig()
+        );
+
+        return $this->configuration;
+    }
+
+    /**
+     * Finds the path to configuration.
+     */
+    protected function getConfigPath()
+    {
+        $cwd = rtrim(getcwd(), DIRECTORY_SEPARATOR);
+        $paths = array_filter(
+            array(
+                $cwd . DIRECTORY_SEPARATOR . 'drupal.yml',
+                $cwd . DIRECTORY_SEPARATOR . 'drupal.yml.dist',
+                $cwd . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'drupal.yml',
+                $cwd . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'drupal.yml.dist',
+                $cwd . DIRECTORY_SEPARATOR . 'cnf' . DIRECTORY_SEPARATOR . 'drupal.yml',
+                $cwd . DIRECTORY_SEPARATOR . 'cnf' . DIRECTORY_SEPARATOR . 'drupal.yml.dist',
+            ),
+            'is_file'
+        );
+
+        if (count($paths)) {
+            return current($paths);
+        }
+
+        return null;
+    }
+
+    /**
+     * Creates configuration loader.
+     *
+     * @return ConfigurationLoader
+     */
+    protected function createConfigLoader()
+    {
+        return new ConfigLoader($this->getConfigPath());
+    }
+
+    /**
+     * Creates configuration loader.
+     *
+     * @return ConfigurationLoader
+     */
+    protected function createConfigTree()
+    {
+        return new ConfigTree();
+    }
+
+    /**
+     * Creates configuration loader.
+     *
+     * @return ConfigurationLoader
+     */
+    protected function createProcessor()
+    {
+        return new Processor();
     }
 
     public function isProductionEnabled()
@@ -86,24 +119,31 @@ class Config
         return $this->productionMode;
     }
 
-    public function getDestination($relDest)
+    public function getDestination()
     {
-        return implode(DIRECTORY_SEPARATOR, [getcwd(), rtrim($relDest, DIRECTORY_SEPARATOR)]);
+        return $this->getConfig()['destination_paths']['root'];
+    }
+
+    public function getFullDestination()
+    {
+        return implode(DIRECTORY_SEPARATOR, [getcwd(), rtrim($this->getDestination(), DIRECTORY_SEPARATOR)]);
     }
 
     public function getPaths()
     {
+        $config = $this->getDestinationConfig();
+
         return [
-            'core'          => $this->destination,
-            'module'        => $this->destination . $this->modulePath,
-            'custom'        => $this->destination . $this->modulePath . '/%s',
-            'theme'         => $this->destination . $this->themePath,
-            'drush'         => $this->destination . $this->drushPath,
-            'profile'       => $this->destination . $this->profilePath,
-            'files-public'  => $this->destination . $this->filesPublicPath,
-            'files-private' => $this->destination . $this->filesPrivatePath,
-            'settings'      => $this->destination . $this->settingsPath,
-            'vendor'        => $this->destination . $this->vendorPath,
+            'core'          => $this->getFullDestination(),
+            'module'        => $config['module'] . DIRECTORY_SEPARATOR . '%s',
+            'custom'        => $config['custom'] . DIRECTORY_SEPARATOR . '%s',
+            'theme'         => $config['theme']  . DIRECTORY_SEPARATOR . '%s',
+            'profile'       => $config['profile']. DIRECTORY_SEPARATOR . '%s',
+            'drush'         => $config['drush'],
+            'files-public'  => $config['files_public'],
+            'files-private' => $config['files_private'],
+            'settings'      => $config['settings'],
+            'vendor'        => $config['vendor'],
             ];
     }
 
---
+++ src/Rootcanal/Config/ConfigTree.php
@@ -0,0 +1,128 @@
+<?php
+
+/*
+ * Configuration Tree loader for Rootcanal.
+ */
+
+namespace Rootcanal\Config;
+
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+
+/**
+ * Loads configuration from different sources.
+ */
+class ConfigTree implements ConfigurationInterface
+{
+    protected $defaults = array(
+        'destination_paths' => array(
+            'root'          => 'www',
+            'module'        => 'sites' . DIRECTORY_SEPARATOR . 'all' . DIRECTORY_SEPARATOR . 'modules',
+            'theme'         => 'sites' . DIRECTORY_SEPARATOR . 'all' . DIRECTORY_SEPARATOR . 'themes',
+            'drush'         => 'sites' . DIRECTORY_SEPARATOR . 'all' . DIRECTORY_SEPARATOR . 'drush',
+            'profile'       => 'profiles',
+            'vendor'        => 'sites' . DIRECTORY_SEPARATOR . 'default' . DIRECTORY_SEPARATOR . 'vendor',
+            'files_public'  => 'sites' . DIRECTORY_SEPARATOR . 'default' . DIRECTORY_SEPARATOR . 'files',
+            'files_private' => 'sites' . DIRECTORY_SEPARATOR . 'default' . DIRECTORY_SEPARATOR . 'files-private',
+            'settings'      => 'sites' . DIRECTORY_SEPARATOR . 'default' . DIRECTORY_SEPARATOR . 'settings.php',
+        ),
+        'source_paths' => array(
+           'source_root'   => '',
+           'files_public'  => 'cnf' . DIRECTORY_SEPARATOR . 'files',
+           'files_private' => 'cnf' . DIRECTORY_SEPARATOR . 'private',
+           'settings'      => 'cnf' . DIRECTORY_SEPARATOR . 'settings.php',
+        ),
+        'finder_settings' => array(
+            'ignore_dirs' => ['vendor', 'cnf'],
+            'custom_file_extensions' => ['php', 'inc', 'module', 'info', 'install'],
+        )
+    );
+
+    /**
+     * Returns the configuration defaults.
+     *
+     * @return array
+     */
+    public function getDefaults()
+    {
+        return $this->defaults;
+    }
+
+    /**
+     * Generates the configuration tree.
+     *
+     * @return NodeInterface
+     */
+    public function getConfigTreeBuilder()
+    {
+        $defaults = $this->defaults;
+        $tree = new TreeBuilder();
+        $rootNode = $tree->root('drupal');
+        $rootNode->children()
+                ->arrayNode('destination_paths')
+                  ->isRequired()
+                  ->cannotBeEmpty()
+                  ->children()
+                    ->scalarNode('root')
+                      ->defaultValue($defaults['destination_paths']['root'])
+                    ->end()
+                    ->scalarNode('module')
+                      ->defaultValue($defaults['destination_paths']['module'])
+                    ->end()
+                    ->scalarNode('theme')
+                      ->defaultValue($defaults['destination_paths']['theme'])
+                    ->end()
+                    ->scalarNode('drush')
+                      ->defaultValue($defaults['destination_paths']['drush'])
+                    ->end()
+                    ->scalarNode('profile')
+                      ->defaultValue($defaults['destination_paths']['profile'])
+                    ->end()
+                    ->scalarNode('vendor')
+                      ->defaultValue($defaults['destination_paths']['vendor'])
+                    ->end()
+                    ->scalarNode('files_public')
+                      ->defaultValue($defaults['destination_paths']['files_public'])
+                    ->end()
+                    ->scalarNode('files_private')
+                      ->defaultValue($defaults['destination_paths']['files_private'])
+                    ->end()
+                    ->scalarNode('settings')
+                      ->defaultValue($defaults['destination_paths']['settings'])
+                    ->end()
+                  ->end()
+              ->end()
+              ->arrayNode('source_paths')
+                  ->isRequired()
+                  ->cannotBeEmpty()
+                  ->children()
+                    ->scalarNode('source_root')
+                      ->defaultValue($defaults['source_paths']['source_root'])
+                    ->end()
+                    ->scalarNode('files_public')
+                      ->defaultValue($defaults['source_paths']['files_public'])
+                    ->end()
+                    ->scalarNode('files_private')
+                      ->defaultValue($defaults['source_paths']['files_private'])
+                    ->end()
+                    ->scalarNode('settings')
+                      ->defaultValue($defaults['source_paths']['settings'])
+                    ->end()
+                 ->end()
+               ->end()
+               ->arrayNode('finder_settings')
+                  ->isRequired()
+                  ->cannotBeEmpty()
+                  ->children()
+                    ->variableNode('ignore_dirs')
+                      ->defaultValue($defaults['finder_settings']['ignore_dirs'])
+                    ->end()
+                    ->variableNode('custom_file_extensions')
+                      ->defaultValue($defaults['finder_settings']['custom_file_extensions'])
+                    ->end()
+                 ->end()
+            ->end();
+
+        return $tree;
+    }
+}
